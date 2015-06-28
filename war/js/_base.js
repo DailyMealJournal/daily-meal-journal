5d3b8612d7060610df41921aeda07f08efee2676
LouizEchi <louizechiverri@gmail.com>
2015-06-29 00:00:23 +0800

    [General]minor changes

--- war/js/_base.js
+++ war/js/_base.js
@@ -1,17 +1,17 @@
 //sets title page
-var page_title = $('page-title').attr('name')
+var page_title = $('pagetitle').attr('name')
 $('title').html(page_title);
 $('page-title').remove()
 
 //handles adding of css
-$('style-url').each(function(){
+$('styleurl').each(function(){
     var style_element = '<link rel="stylesheet" type="text/css" href="../css/'+ $(this).attr('url') +'"></link>';
     $('stylesheet').append(style_element);
     $(this).remove();
 });
 
 //handles adding of javascripts
-$('js-script').each(function(){
+$('jsscript').each(function(){
     var js_element = '<script type="text/javascript" src="../js/'+ $(this).attr('src') +'"></script>';
     $('scripts').append(js_element);
     $(this).remove();
