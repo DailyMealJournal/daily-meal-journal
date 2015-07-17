59e4d88d7943ae8a2fc565e244f7f3d3efa745c4
Louiz Echiverri <louizechiverri@gmail.com>
2015-07-17 14:56:34 +0800

    fixed reclicking of view type

--- war/js/calowrie/meal_management/angular_index.js
+++ war/js/calowrie/meal_management/angular_index.js
@@ -71,27 +71,31 @@ meal_management.directive('backImg', function(){
 
  $(document).ready(function(){
  	$('#list_meals').hide();
- 	$('.grid-view').attr('disabled', 'disabled')
+ 	$('.grid-view').addClass('active')
  	$('#loader').hide();
 	$('.grid-view').click(function(e){
 		e.preventDefault();
-		$(this).addClass('blue-text')
-		$(this).attr('disabled','disabled');
-		$('.list-view').removeClass('blue-text');
-		$('.list-view').removeAttr('disabled');
-		$('#list_meals').fadeOut(100);	
-		$('#loader').fadeIn(500).delay(800).fadeOut(100);
-		$('#grid_meals').delay(1350).fadeIn(200);
+		if(!$(this).hasClass('active')) {
+			$(this).addClass('blue-text')
+			$(this).addClass('active');
+			$('.list-view').removeClass('blue-text');
+			$('.list-view').removeClass('active');
+			$('#list_meals').fadeOut(100);	
+			$('#loader').fadeIn(500).delay(800).fadeOut(100);
+			$('#grid_meals').delay(1350).fadeIn(200);
+		}
 	});
 
 	$('.list-view').click(function(e){
 		e.preventDefault();
-		$(this).addClass('blue-text')
-		$(this).attr('disabled','disabled');
-		$('.grid-view').removeClass('blue-text');
-		$('.grid-view').removeAttr('disabled');
-		$('#grid_meals').fadeOut(100);
-		$('#loader').fadeIn(500).delay(800).fadeOut(100);
-		$('#list_meals').delay(1350).fadeIn(200);
+		if(!$(this).hasClass('active')) {
+			$(this).addClass('blue-text')
+			$(this).addClass('active');
+			$('.grid-view').removeClass('blue-text');
+			$('.grid-view').removeClass('active');
+			$('#grid_meals').fadeOut(100);
+			$('#loader').fadeIn(500).delay(800).fadeOut(100);
+			$('#list_meals').delay(1350).fadeIn(200);
+		}
 	})
 })
