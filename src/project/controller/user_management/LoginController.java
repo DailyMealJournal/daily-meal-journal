fe32f5edbd57e7b1fd1be6a3a3cbfc241d43ecc8
Rafael Bacus <talatsu92@gmail.com>
2015-08-12 11:46:38 +0800

    Merged

--- src/project/controller/user_management/LoginController.java
+++ src/project/controller/user_management/LoginController.java
@@ -1,5 +1,6 @@
 package project.controller.user_management;
 
+
 import java.util.Map;
 
 import org.slim3.controller.Navigation;
@@ -20,14 +21,14 @@ public class LoginController extends APIController {
     @Override
     protected Navigation run() throws Exception {
         UserDto userDto = new UserDto();
-
+        sessionScope("user", null);
         JSONObject json = null;
         JSONObject error = null;
         try {
                 Map<String,Object> input = new RequestMap(this.request);
                 BeanUtil.copy(input, userDto);
-                json = new JSONObject(userDto);
                 Entity user = service.getEntity(userDto);
+                json = new JSONObject(user);
                 if(user == null) {
                     error = new JSONObject();
                     error.put("message","Invalid Username and/or Password");
@@ -35,8 +36,8 @@ public class LoginController extends APIController {
                 }
         } catch (Exception e) {
             error = new JSONObject();
-            error.put("message","Error");
-            error.put("code", "1002");
+            error.put("message","Invalid Username and/or Password");
+            error.put("code", "1001");
             if(json == null) {
                 json = new JSONObject();
             }
@@ -48,8 +49,12 @@ public class LoginController extends APIController {
             response.getWriter().print(error);
         } else {
             response.getWriter().print(json);
+<<<<<<< HEAD
             sessionScope("user", "duh");
             System.out.println("LoginController");
+=======
+            sessionScope("user", setSession(json));
+>>>>>>> 4aab2f354e80f792f32ac5dfeef6b18a7db51131
         }
         
         return null;
